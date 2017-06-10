27c9aedb7abb927999bee2d65fc26ccfa410b2c3
codefinite <gh@moonlitdesign.co.uk>
2017-06-10 05:46:22 +0100

    Initial commit

---
+++ superwide.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function superWideZoom(request, sender, sendResponse) {
+    var head = document.head || document.getElementsByTagName('head')[0],
+        superWideZoomStyle = document.getElementById('superWideZoomStyle'),
+        on = false;
+    if (superWideZoomStyle === null) {
+        superWideZoomStyle = document.createElement('style');
+        superWideZoomStyle.setAttribute('id', 'superWideZoomStyle');
+        superWideZoomStyle.appendChild(
+            document.createTextNode(
+                '#netflix-player video[src], #dv-web-player video[src], #movie_player.ytp-fullscreen video[src] { ' +
+                'height: 145vh !important;' +
+                'width: 100vw !important;' +
+                'left: 0 !important;' +
+                'top: -22.5vh !important;' +
+                '}'
+            )
+        );
+        head.appendChild(superWideZoomStyle);
+        on = true;
+    } else {
+        superWideZoomStyle.remove();
+    }
+    sendResponse(on);
+}
+
+browser.runtime.onMessage.addListener(superWideZoom);
