8f9cc6870e1c9e88db189bd3bab65f19c15207cb
Alaric von Teplitz <alaric2000@gmail.com>
2017-06-10 14:22:49 -0600

    Added newline at EOF, replaced single and double quites with backticks, replaced function keyword with =>, replaced var with const.

--- superwide.js
+++ superwide.js
@@ -1,20 +1,20 @@
-"use strict";
+`use strict`;
 
 function superWideZoom(request, sender, sendResponse) {
-    var head = document.head || document.getElementsByTagName('head')[0],
-        superWideZoomStyle = document.getElementById('superWideZoomStyle'),
+    const head = document.head || document.getElementsByTagName(`head`)[0],
+        superWideZoomStyle = document.getElementById(`superWideZoomStyle`),
         on = false;
     if (superWideZoomStyle === null) {
-        superWideZoomStyle = document.createElement('style');
-        superWideZoomStyle.setAttribute('id', 'superWideZoomStyle');
+        superWideZoomStyle = document.createElement(`style`);
+        superWideZoomStyle.setAttribute(`id`, `superWideZoomStyle`);
         superWideZoomStyle.appendChild(
             document.createTextNode(
-                '#netflix-player video[src], #dv-web-player video[src], #movie_player.ytp-fullscreen video[src] { ' +
-                'height: 145vh !important;' +
-                'width: 100vw !important;' +
-                'left: 0 !important;' +
-                'top: -22.5vh !important;' +
-                '}'
+                `#netflix-player video[src], #dv-web-player video[src], #movie_player.ytp-fullscreen video[src] {
+                    height: 145vh !important;
+                    width: 100vw !important;
+                    left: 0 !important;
+                    top: -22.5vh !important;
+                }`
             )
         );
         head.appendChild(superWideZoomStyle);
